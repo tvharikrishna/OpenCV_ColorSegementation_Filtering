17b46790397d9e0f71d59425f795e8e939c9f2ee
tvharikrishna <tvharikrishna74@gmail.com>
2023-11-25 20:51:51 -0500

    final update to repo: all checks clear

--- scripts/color_segementation.py
+++ scripts/color_segementation.py
@@ -1,90 +1,114 @@
+"""
+Author: Hari Krishna
+Github: tvharikrishna
+Linkedin: www.linkedin.com/in/tvharikrishna
+"""
+
+
+
 import cv2
 import numpy as np
 import os
 
-def color_spaces(colorcode: str, image_path: str, save_path: str):
+class ColorSpaceProcessor:
+    """
+    A class to process an image in different color spaces and extract specified colors.
+    """
 
-    # Load the image
-    frame = cv2.imread(image_path)
+    def __init__(self, image_path, save_path):
+        """
+        Initialize the processor with the image path and save path.
 
-    if frame is None:
-        print('Image not found')
-        return
+        :param image_path: Path to the image file.
+        :param save_path: Path where the processed images will be saved.
+        """
+        self.image_path = image_path
+        self.save_path = save_path
+        self.frame = cv2.imread(image_path)
+        if self.frame is None:
+            raise FileNotFoundError("Image not found at provided path.")
+        self.color_ranges = {
 
-    # Define color ranges for filtering with expanded blue range
-    color_ranges = {
-        'blue_lower': ([90, 150, 150], [130, 255, 255]),  
-        'blue_upper': ([80, 150, 150], [100, 255, 255]),
+            # Filter out blue color and range
+            'blue_lower': ([90, 150, 150], [130, 255, 255]),  
+            'blue_upper': ([80, 150, 150], [100, 255, 255]),
 
-        'green': ([67, 200, 200], [87, 255, 255]),
+            # Filter out green color
+            'green': ([67, 200, 200], [87, 255, 255]),
 
-        'yellow': ([17, 132, 200], [37, 232, 255]),
+            # Filter out yellow color
+            'yellow': ([17, 132, 200], [37, 232, 255]),
 
-        'red_lower': ([0, 202, 185], [15, 255, 255]),
-        'red_upper': ([160, 202, 185], [179, 255, 255])
-    }
+            # Filter out red color
+            'red_lower': ([0, 202, 185], [15, 255, 255]),
+            'red_upper': ([160, 202, 185], [179, 255, 255])
+        }
 
-    # Convert to different color spaces
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    def process_color_space(self, colorcode):
+        """
+        Process the image to extract the specified color and save the results.
 
-    # Ensure the save path exists
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
+        :param colorcode: The color to be extracted from the image.
+        """
+        # Convert to different color spaces
+        gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
 
-    # Function to save images without adding text
-    def save_image(image, save_name):
-        cv2.imwrite(os.path.join(save_path, save_name), image)
+        # Ensure the save path exists
+        if not os.path.exists(self.save_path):
+            os.makedirs(self.save_path)
 
-    # Apply color filter based on the colorcode
-    if colorcode == 'blue':
-        # For blue, we need to combine two ranges
-        lower_blue1, upper_blue1 = color_ranges['blue_lower']
-        lower_blue2, upper_blue2 = color_ranges['blue_upper']
-        lower_blue1 = np.array(lower_blue1, dtype="uint8")
-        upper_blue1 = np.array(upper_blue1, dtype="uint8")
-        lower_blue2 = np.array(lower_blue2, dtype="uint8")
-        upper_blue2 = np.array(upper_blue2, dtype="uint8")
-        mask1 = cv2.inRange(hsv, lower_blue1, upper_blue1)
-        mask2 = cv2.inRange(hsv, lower_blue2, upper_blue2)
-        mask = cv2.bitwise_or(mask1, mask2)
-    elif colorcode == 'red':
-        # For red, we need to combine two ranges
-        lower_red1, upper_red1 = color_ranges['red_lower']
-        lower_red2, upper_red2 = color_ranges['red_upper']
-        lower_red1 = np.array(lower_red1, dtype="uint8")
-        upper_red1 = np.array(upper_red1, dtype="uint8")
-        lower_red2 = np.array(lower_red2, dtype="uint8")
-        upper_red2 = np.array(upper_red2, dtype="uint8")
-        mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
-        mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
-        mask = cv2.bitwise_or(mask1, mask2)
-    else:
-        # For other colors, use the single range
-        lower, upper = color_ranges[colorcode]
-        lower = np.array(lower, dtype="uint8")
-        upper = np.array(upper, dtype="uint8")
-        mask = cv2.inRange(hsv, lower, upper)
+        # Apply color filter based on the colorcode
+        mask = self.apply_color_filter(hsv, colorcode)
 
-    filtered = cv2.bitwise_and(frame, frame, mask=mask)
+        filtered = cv2.bitwise_and(self.frame, self.frame, mask=mask)
 
-    # Save all frames without text
-    save_image(frame, '1.Original_Image.png')
-    save_image(gray, '2.Grayscale_Image.png')
-    save_image(hsv, '3.HSV_Image.png')
-    save_image(mask, '4.Threshold_Image.png')
-    save_image(filtered, colorcode + '_Filtered_Color_Image.png')
+        # Save all frames without text
+        self.save_image(self.frame, '1.Original_Image.png')
+        self.save_image(gray, '2.Grayscale_Image.png')
+        self.save_image(hsv, '3.HSV_Image.png')
+        self.save_image(mask, '4.Threshold_Image.png')
+        self.save_image(filtered, f'{colorcode}_Filtered_Color_Image.png')
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        cv2.destroyAllWindows()
+
+    def apply_color_filter(self, hsv, colorcode):
+        """
+        Apply the color filter based on the specified colorcode.
+
+        :param hsv: The HSV representation of the image.
+        :param colorcode: The color to be filtered.
+        :return: The mask after applying the color filter.
+        """
+        if colorcode in ['blue', 'red']:
+            lower1, upper1 = self.color_ranges[f'{colorcode}_lower']
+            lower2, upper2 = self.color_ranges[f'{colorcode}_upper']
+            mask1 = cv2.inRange(hsv, np.array(lower1, dtype="uint8"), np.array(upper1, dtype="uint8"))
+            mask2 = cv2.inRange(hsv, np.array(lower2, dtype="uint8"), np.array(upper2, dtype="uint8"))
+            return cv2.bitwise_or(mask1, mask2)
+        else:
+            lower, upper = self.color_ranges[colorcode]
+            return cv2.inRange(hsv, np.array(lower, dtype="uint8"), np.array(upper, dtype="uint8"))
+
+    def save_image(self, image, save_name):
+        """
+        Save the given image with the specified filename.
+
+        :param image: The image to be saved.
+        :param save_name: The filename for the saved image.
+        """
+        cv2.imwrite(os.path.join(self.save_path, save_name), image)
+
 
 if __name__ == "__main__":
-
     # Path to your Image
-    image_path = 'data\original_Image.png'  # Replace with the actual path to your image
+    image_path = 'scripts/data/original_Image.png'
     
     # Directory to save the images
-    save_path = 'generated_images' # Replace with the actual path to your save directory
+    save_path = 'scripts/generated_images'
     
-    # Enter the color that you want to filter out 
-    color_spaces('red', image_path, save_path)  # Use 'green' as the colorcode to test the green segmentation
+    # Create an instance of ColorSpaceProcessor
+    processor = ColorSpaceProcessor(image_path, save_path)
+    
+    # Enter the color that you want to filter out
+    processor.process_color_space('red')
